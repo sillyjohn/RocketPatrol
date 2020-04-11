b74ba9ab4209b20271e28d08d21c28dc1f090d10
John Tsoi <ctsoi1@ucsc.edu>
2020-04-10 17:24:14 -0700

    commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,10 +2,11 @@ class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
     }
-    create(){
-        console.log(this);
+    create() {
+        //display menu
         this.add.text(20,20,"Rocket Patrol Menu");
-
+        //launch the next scene
+        this.scene.start("playScene")
     }
 
 }
